cf20861930e7e0b1bf1580a11a3d2b6f99fad6f0
sasi <sasi@DESKTOP-NP667UJ>
2020-04-18 17:56:57 +0530

    update

--- src/main/java/com/ecommerce/applicaton/eurekaServer/ServerApplication.java
+++ src/main/java/com/ecommerce/applicaton/eurekaServer/ServerApplication.java
@@ -3,9 +3,11 @@ package com.ecommerce.applicaton.eurekaServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableZuulProxy
 public class ServerApplication {
 
 	public static void main(String[] args) {
